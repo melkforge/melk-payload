3218549f0f63d6dad59651fda21c2fa4f54c9e5d
Evin <evin4369@gmail.com>
2025-05-06 14:59:25 -0700

    wrote a filtering function to handle product filtering

--- src/app/(frontend)/map/page.tsx
+++ src/app/(frontend)/map/page.tsx
@@ -3,7 +3,8 @@
 import React, { useEffect, useRef } from "react";
 import mapboxgl, { GeoJSONSource } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-
+// import { stringify } from "qs-esm";
+// import { Where } from "payload";
 
 interface MovingObject {
   id: number;
@@ -22,64 +23,103 @@ const MapComponent: React.FC = () => {
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
     if (mapContainer.current) {
-        const map = new mapboxgl.Map({
-            container: mapContainer.current,
-            center: [-123.1207, 49.2827],
-            zoom: 10,
-            maxZoom: 15,
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        center: [-123.1207, 49.2827],
+        zoom: 10,
+        maxZoom: 15,
+      });
+
+      let formattedLocations: string[];
+      let locationOBJ: any;
+
+
+      //-----------------------------------
+      // let item = "Evin"
+
+      // const getProducts = async (item: string) => {
+
+      //   const product_query: Where = {
+      //     'product_id.product_name': {
+      //       equals: item,
+      //     },
+      //   }
+
+      //   let stringifiedQuery = stringify(
+      //     {
+      //       where: product_query,
+      //     },
+      //     { addQueryPrefix: true },
+      //   )
+
+      //   const product_response = await fetch(
+      //     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/product_availability${stringifiedQuery}`,
+      //   );
+
+      //   const product_data = await product_response.json();
+      //   locationOBJ = product_data.docs;
+
+      //   formattedLocations = locationOBJ.map(
+      //     (loc: any) => `${loc.location_id.FINAL_NAME}, ${loc.location_id.Address_by_ID}, ${loc.location_id.City_by_ID}, ${loc.location_id.Province}`
+      //   );
+
+      //   console.log(formattedLocations);
+
+      // }
+      // getProducts();
+
+
+
+
+      //-----------------------------------
+
+      fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations`)
+        .then((res) => res.json())
+        .then((data) => {
+          const locations = data.docs;
+          console.log(locations);
+          locationOBJ = locations;
+          formattedLocations = locations.map(
+            (loc: any) => `${loc.address_line1}, ${loc.city}`
+          );
+
+
+          for (let i = 0; i < formattedLocations.length; i++) {
+            fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(formattedLocations[i] as string)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
+              .then(res => res.json())
+              .then(res => {
+                const [longitude, latitude] = res.features[0].geometry.coordinates;
+
+                const marker = new mapboxgl.Marker()
+                  .setLngLat([longitude, latitude])
+                  .setPopup(new mapboxgl.Popup().setHTML(`<div style="color: red;">${locationOBJ[i].store_name}<br>${locationOBJ[i].address_line1}</div>`))
+                  .addTo(map)
+              });
+          }
+
+          // formattedLocations.forEach((address) => {
+
+          //     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
+          //         .then(res => res.json())
+          //         .then(res => {
+          //             const [longitude, latitude] = res.features[0].geometry.coordinates;
+
+          //             const marker = new mapboxgl.Marker()
+          //                 .setLngLat([longitude, latitude])
+          //                 .setPopup(new mapboxgl.Popup().setHTML(`<h3>hello</h3>`))
+          //                 .addTo(map)
+
+          //         })
+          // });
+          // Add zoom controls
+          map.addControl(new mapboxgl.NavigationControl(), "top-left");
+
         });
 
-        let formattedLocations: string[];
-        let locationOBJ: any;
-
-        fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations`)
-            .then((res) => res.json())
-            .then((data) => {
-                const locations = data.docs;
-                console.log(locations);
-                locationOBJ = locations;
-                formattedLocations = locations.map(
-                    (loc: any) => `${loc.address_line1}, ${loc.city}`
-                );
-
-
-                for (let i = 0; i < formattedLocations.length; i++) {
-                    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(formattedLocations[i] as string)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
-                        .then(res => res.json())
-                        .then(res => {
-                            const [longitude, latitude] = res.features[0].geometry.coordinates;
-
-                            const marker = new mapboxgl.Marker()
-                                .setLngLat([longitude, latitude])
-                                .setPopup(new mapboxgl.Popup().setHTML(`<div style="color: red;">${locationOBJ[i].store_name}<br>${locationOBJ[i].address_line1}</div>`))
-                                .addTo(map)
-                        });
-                }
-
-                // formattedLocations.forEach((address) => {
-
-                //     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
-                //         .then(res => res.json())
-                //         .then(res => {
-                //             const [longitude, latitude] = res.features[0].geometry.coordinates;
-
-                //             const marker = new mapboxgl.Marker()
-                //                 .setLngLat([longitude, latitude])
-                //                 .setPopup(new mapboxgl.Popup().setHTML(`<h3>hello</h3>`))
-                //                 .addTo(map)
-
-                //         })
-                // });
-
-                // Add zoom controls
-                map.addControl(new mapboxgl.NavigationControl(), "top-left");
-
-            });
-
-        // Clean up on unmount
-        return () => map.remove();
+      // Clean up on unmount
+      return () => map.remove();
     }
-}, []);
+  }, []);
 
   return (
     <div
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,7 +3,40 @@
 import React, { useEffect, useRef } from "react";
 import mapboxgl, { GeoJSONSource } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { stringify } from "qs-esm";
+import { Where } from "payload";
 
+// FILTERING FUNCTION, UNUESD FOR NOW
+const getProducts = async (item: string) => {
+
+  let formattedLocations: string[];
+  let locationOBJ: any;
+
+  const product_query: Where = {
+    'product_id.product_name': {
+      equals: item,
+    },
+  }
+
+  let stringifiedQuery = stringify(
+    {
+      where: product_query,
+    },
+    { addQueryPrefix: true },
+  )
+
+  const product_response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/product_availability${stringifiedQuery}`,
+  );
+
+  const product_data = await product_response.json();
+  locationOBJ = product_data.docs;
+
+  formattedLocations = locationOBJ.map(
+    (loc: any) => `${loc.location_id.FINAL_NAME}, ${loc.location_id.Address_by_ID}, ${loc.location_id.City_by_ID}, ${loc.location_id.Province}`
+  );
+  console.log(formattedLocations);
+}
 
 interface MovingObject {
   id: number;
@@ -22,80 +55,72 @@ const MapComponent: React.FC = () => {
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
     if (mapContainer.current) {
-        const map = new mapboxgl.Map({
-            container: mapContainer.current,
-            center: [-123.1207, 49.2827],
-            zoom: 10,
-            maxZoom: 15,
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        center: [-123.1207, 49.2827],
+        zoom: 10,
+        maxZoom: 15,
+      });
+
+      let formattedLocations: string[];
+      let locationOBJ: any;
+
+      fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations`)
+        .then((res) => res.json())
+        .then((data) => {
+          const locations = data.docs;
+          console.log(locations);
+          locationOBJ = locations;
+          formattedLocations = locations.map(
+            (loc: any) => `${loc.address_line1}, ${loc.city}`
+          );
+
+
+          for (let i = 0; i < formattedLocations.length; i++) {
+            fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(formattedLocations[i] as string)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
+              .then(res => res.json())
+              .then(res => {
+                const [longitude, latitude] = res.features[0].geometry.coordinates;
+
+                const marker = new mapboxgl.Marker()
+                  .setLngLat([longitude, latitude])
+                  .setPopup(new mapboxgl.Popup().setHTML(`<div style="color: red;">${locationOBJ[i].store_name}<br>${locationOBJ[i].address_line1}</div>`))
+                  .addTo(map)
+              });
+          }
+
+          // formattedLocations.forEach((address) => {
+
+          //     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
+          //         .then(res => res.json())
+          //         .then(res => {
+          //             const [longitude, latitude] = res.features[0].geometry.coordinates;
+
+          //             const marker = new mapboxgl.Marker()
+          //                 .setLngLat([longitude, latitude])
+          //                 .setPopup(new mapboxgl.Popup().setHTML(`<h3>hello</h3>`))
+          //                 .addTo(map)
+
+          //         })
+          // });
+
+          // Add zoom controls
+          map.addControl(new mapboxgl.NavigationControl(), "top-left");
+
         });
 
-        let formattedLocations: string[];
-        let locationOBJ: any;
-
-        fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations`)
-            .then((res) => res.json())
-            .then((data) => {
-                const locations = data.docs;
-                
-                locationOBJ = locations;
-
-                //console.log(locationOBJ);
-                formattedLocations = locationOBJ.map(
-                    (loc: any) => `${loc.FINAL_NAME}, ${loc.Address_by_ID}, ${loc.City_by_ID}, ${loc.Province}`
-                );
-
-                //console.log(formattedLocations);
-              
-
-                for (let i = 0; i < formattedLocations.length; i++) {
-                    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(formattedLocations[i] as string)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
-                        .then(res => res.json())
-                        .then(res => {
-                            if(!res.features || res.features.length === 0) {
-                                console.error(`no results for: ${formattedLocations[i]}`);
-                                return;
-                            }
-                            const [longitude, latitude] = res.features[0].geometry.coordinates;
-
-                            const marker = new mapboxgl.Marker()
-                                .setLngLat([longitude, latitude])
-                                .setPopup(new mapboxgl.Popup().setHTML(`<div style="color: black;">${locationOBJ[i].FINAL_NAME}<br>${locationOBJ[i].Address_by_ID}<br>${locationOBJ[i].City_by_ID}<br>${locationOBJ[i].Province}</div>`))
-                                .addTo(map)
-                        });
-                }
-
-                // formattedLocations.forEach((address) => {
-
-                //     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
-                //         .then(res => res.json())
-                //         .then(res => {
-                //             const [longitude, latitude] = res.features[0].geometry.coordinates;
-
-                //             const marker = new mapboxgl.Marker()
-                //                 .setLngLat([longitude, latitude])
-                //                 .setPopup(new mapboxgl.Popup().setHTML(`<h3>hello</h3>`))
-                //                 .addTo(map)
-
-                //         })
-                // });
-
-                // Add zoom controls
-                map.addControl(new mapboxgl.NavigationControl(), "top-left");
-
-            });
-
-        // Clean up on unmount
-        return () => map.remove();
+      // Clean up on unmount
+      return () => map.remove();
     }
-}, []);
+  }, []);
 
   return (
     <div
-        ref={mapContainer}
-        style={{
+      ref={mapContainer}
+      style={{
         width: "100%",
         height: "100%",
-        }}
+      }}
     />
   );
 };
